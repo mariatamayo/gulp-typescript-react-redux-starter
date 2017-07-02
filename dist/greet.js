4ad35b5721e5b38d5d0d81a01601ef2e0b68b083
Maria Jose Tamayo <mariajosetamayo@Marias-MacBook-Pro.local>
2017-07-02 10:31:18 -0600

    gulp working with ts and js

---
+++ dist/greet.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayHello(name) {
+    return "Hello from " + name;
+}
+exports.sayHello = sayHello;
+function sayGoodbye(name) {
+    return "Goodbye from " + name;
+}
+exports.sayGoodbye = sayGoodbye;
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdyZWV0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsa0JBQXlCLElBQVk7SUFDakMsTUFBTSxDQUFDLGdCQUFjLElBQU0sQ0FBQztBQUNoQyxDQUFDO0FBRkQsNEJBRUM7QUFFRCxvQkFBMkIsSUFBWTtJQUNuQyxNQUFNLENBQUMsa0JBQWdCLElBQU0sQ0FBQztBQUNsQyxDQUFDO0FBRkQsZ0NBRUMiLCJmaWxlIjoiZ3JlZXQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZnVuY3Rpb24gc2F5SGVsbG8obmFtZTogc3RyaW5nKSB7XG4gICAgcmV0dXJuIGBIZWxsbyBmcm9tICR7bmFtZX1gO1xufVxuXG5leHBvcnQgZnVuY3Rpb24gc2F5R29vZGJ5ZShuYW1lOiBzdHJpbmcpIHtcbiAgICByZXR1cm4gYEdvb2RieWUgZnJvbSAke25hbWV9YDtcbn1cbiJdfQ==
---
+++ src/greet.ts
@@ -0,0 +1,7 @@
+export function sayHello(name: string) {
+    return `Hello from ${name}`;
+}
+
+export function sayGoodbye(name: string) {
+    return `Goodbye from ${name}`;
+}
